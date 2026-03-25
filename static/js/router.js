e753939aded6fb1ae70ae07f3f132d768b1da27d
badil3in <badil3in@googlemail.com>
2026-03-25 17:53:54 +0100

    SPA, template with nunjucks, created JSON file

---
+++ static/js/router.js
@@ -0,0 +1,38 @@
+// configure flags
+nunjucks.configure('/templates', { 
+    autoescape: true,
+    async: true 
+});
+
+// render function
+
+async function render() {
+    const page = location.hash.replace('#/', '') || 'index';
+
+    // JS object - templating context
+    let context = {};
+
+    if (page === "modules") {
+        // fetch modules from JSON file
+        context.modules = await fetch('/static/JSON/modules.JSON').then(r => r.json())
+    }
+    console.log("Modules JSON:", context.modules);
+
+    // context an nunjucks übergeben
+    const html = await nunjucks.render(`${page}.njk`, context);
+    document.getElementById('app').innerHTML = html;
+
+    activeNav(page);
+}
+
+// toggle nav active
+
+function activeNav(page) {
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.classList.toggle('active', link.getAttribute('href') === `#/${page}`);
+    });
+}
+
+
+window.addEventListener('hashchange', render);
+render();
