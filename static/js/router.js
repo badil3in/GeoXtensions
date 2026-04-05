d7572b94bda05bd8f9cd9f31ac764ef1cd0723cd
badil3in <badil3in@googlemail.com>
2026-04-05 14:34:33 +0200

    switch paths public

--- static/js/router.js
+++ static/js/router.js
@@ -15,9 +15,9 @@ async function render() {
     if (page === "modules"){
         // fetch modules from JSON file
         // Githubpages
-        // context.modules = await fetch('https://badil3in.github.io/geoxtensions/static/JSON/modules.JSON').then(r => r.json())
+        context.modules = await fetch('https://badil3in.github.io/geoxtensions/static/JSON/modules.JSON').then(r => r.json())
         // local
-        context = await fetch('/static/JSON/modules.JSON').then(r => r.json())
+        // context = await fetch('/static/JSON/modules.JSON').then(r => r.json())
     }
     console.log("pre data :", context.modules)
 
@@ -79,9 +79,9 @@ async function search(query) {
     let results = [];
 
     // public
-    // data.modules = await fetch('https://badil3in.github.io/geoxtensions/static/JSON/modules.JSON')
+    data.modules = await fetch('https://badil3in.github.io/geoxtensions/static/JSON/modules.JSON')
     // local
-    data.modules = await fetch('/static/JSON/modules.JSON')
+    // data.modules = await fetch('/static/JSON/modules.JSON')
         .then(r => r.json());
 
     console.log('data: ', data);    
