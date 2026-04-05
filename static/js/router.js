612ffac443852a4d72fe8da0214655fdbd83a8f0
badil3in <badil3in@googlemail.com>
2026-04-05 12:18:31 +0200

    implement module search

--- static/js/router.js
+++ static/js/router.js
@@ -6,23 +6,31 @@ nunjucks.configure('./templates', {
 
 // render function
 async function render() {
+    // page depending on # in url
     const page = location.hash.replace('#/', '') || 'index';
 
     // JS object - templating context
     let context = {};
 
-    if (page === "modules") {
+    if (page === "modules"){
         // fetch modules from JSON file
         // Githubpages
-        context.modules = await fetch('https://badil3in.github.io/geoxtensions/static/JSON/modules.JSON').then(r => r.json())
+        // context.modules = await fetch('https://badil3in.github.io/geoxtensions/static/JSON/modules.JSON').then(r => r.json())
         // local
-        // context.modules = await fetch('/static/JSON/modules.JSON').then(r => r.json())
+        context = await fetch('/static/JSON/modules.JSON').then(r => r.json())
     }
 
     // context an nunjucks übergeben
-    const html = await nunjucks.render(`${page}.njk`, context, function (err, html) {
+    const html = await nunjucks.render(`${page}.njk`, {
+        isSearch: false,
+        modules: context}, function (err, html) {
         document.getElementById('app').innerHTML = html;
         activeNav(page);
+        console.log("err :", err)
+        console.log("data :", {
+            isSearch: false,
+            modules: context.modules})
+
     });
 }
 
@@ -49,6 +57,82 @@ document.addEventListener('click', function(event) {
         toggler.click();
     }
 })
+
+// on submit search
+let form = document.querySelector("form[role=search]");
+let input = document.getElementById('search');
+
+form.addEventListener('submit', function(event) {
+    let query = input.value;
+    event.preventDefault();
+    if (!query) {
+        render();
+    }
+    else {
+        search(query);        
+    }
+})
+
+async function search(query) {
+    let data = [];
+    let results = [];
+    data.modules = await fetch('/static/JSON/modules.JSON')
+        .then(r => r.json());
+
+    console.log('data: ', data);    
+    console.log('query: ', query);
+
+    for(item of data.modules) {
+        console.log("item.name: ", item.name);
+        if (item.name.toLowerCase().includes(query.toLowerCase())) {
+            results.push(item);
+            console.log("results name:", results);
+            continue;
+        }
+        else if (item.summary.toLowerCase().includes(query.toLowerCase())){
+            results.push(item);
+            console.log("results summary:", results);
+            continue;
+        }
+        for(desc of item.description) {
+            for (sub of desc.subtext) {
+                if(sub.toLowerCase().includes(query.toLowerCase())) {
+                    results.push(item);
+                    console.log("results subtext:", results);
+                    continue;   
+                    }            
+                }
+            if (desc.text.toLowerCase().includes(query.toLowerCase())) {
+                results.push(item);
+                console.log("results text:", results);
+                continue;            
+            }
+            else {
+                console.log("no results :", results);
+            }
+        }
+    }
+        
+        // else if (item.description.includes(query)) {
+        //     results.push(item);
+        //     console.log("results if2:", results);
+        //     continue;
+        // // }
+        // else {
+        //     console.log("no results :", results);
+        // }
+    // }
+    nunjucks.render('modules.njk', {
+        isSearch: true,
+        modules: results
+        }, function (err, html) {
+            if (err){
+                console.log("err :", err);
+                return;
+            }
+        document.getElementById('app').innerHTML = html;
+      });
+}
 
 window.addEventListener('hashchange', render);
 render();
